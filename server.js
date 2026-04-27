b0a9b1dd4b822002e01733d0578f37e8863ee8cf
MariaKanteliotis <mik5633@psu.edu>
2026-04-26 23:17:56 -0400

    Change quotes from single to double in server.js
    Updated quotes in server.js from single to double.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 const FILE = 'orders.json';
 
